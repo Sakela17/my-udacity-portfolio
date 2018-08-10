0f9daa9a1aea1d1ad657e1f80de1dfceba3061e0
Marina Valiquette <marina.valiquette@gmail.com>
2018-08-10 19:21:51 -0400

    Add about section. Update screenshots

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.querySelector(".scroll-top").style.display = "block";
+		const el = document.querySelector(".scroll-top");
+		el.addEventListener("click", topFunction, false);
+
+	} else {
+		document.querySelector(".scroll-top").style.display = "none";
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+// add event listener to table
